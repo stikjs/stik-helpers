f5042f11a221c47645a5fd5056cc8194db3ab76f
Lukas Alexandre <lukas@codelogic.me>
2014-04-19 20:02:00 +0200

    Extracting from stik core

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt){
+  var srcFiles = [
+    'src/helpers.js',
+    'src/utils.js'
+  ];
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: '<%= pkg.banner.divider %>' +
+              '<%= pkg.banner.project %>' +
+              '<%= pkg.banner.copyright %>' +
+              '<%= pkg.banner.license %>' +
+              '<%= pkg.banner.licenseLink %>' +
+              '<%= pkg.banner.divider %>' +
+              '\n' +
+              '// Version: <%= pkg.version %> | From: <%= grunt.template.today("dd-mm-yyyy") %>\n\n'
+    },
+    jasmine: {
+      src: [
+        'node_modules/stik.js/stik.js',
+        'node_modules/stik.js/stik-labs.js'
+      ].concat(srcFiles),
+      options: {
+        specs: '*_spec.js'
+      }
+    },
+    concat: {
+      options: {
+        separator: '\n',
+        banner: '<%= meta.banner %>'
+      },
+      src: {
+        src: srcFiles,
+        dest: '<%= pkg.name %>'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      stik: {
+        files: {
+         'stik-helpers.min.js': ['stik-helpers.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('test', 'jasmine');
+  grunt.registerTask('pack', ['concat', 'uglify']);
+};
---
+++ stik-helpers.js
@@ -0,0 +1,98 @@
+// ==========================================================================
+// Project:   Stik Helpers - Functional helpers for Stik.js
+// Copyright: Copyright 2013-2014 Lukas Alexandre
+// License:   Licensed under MIT license
+//            See https://github.com/stikjs/stik-helpers/blob/master/LICENSE
+// ==========================================================================
+
+// Version: 1.0.0 | From: 19-04-2014
+
+(function(){
+  var helpers = {},
+      modules = {},
+      tmpDependencies = {};
+
+  window.stik.helper = function helper( as, func ){
+    if ( !as ) { throw "Stik: Helper needs a name"; }
+    if ( !func || typeof func !== "function" ) { throw "Stik: Helper needs a function"; }
+
+    modules[ as ] = window.stik.injectable({
+      module: func,
+      resolvable: true
+    });
+    helpers[ as ] = function(){
+      var func = modules[ as ].resolve( withDependencies() );
+      return func.apply( {}, arguments );
+    };
+
+    return helpers[ as ];
+  };
+
+  function withDependencies(){
+    for ( var name in modules ) {
+      if ( !tmpDependencies.hasOwnProperty( name ) ) {
+        tmpDependencies[ name ] = modules[ name ];
+      }
+    }
+
+    return tmpDependencies;
+  }
+
+  helpers.pushDoubles = function pushDoubles( doubles ){
+    for ( var name in doubles ) {
+      tmpDependencies[ name ] = window.stik.injectable({
+        module: doubles[ name ]
+      });
+    }
+  };
+
+  helpers.cleanDoubles = function cleanDoubles(){
+    tmpDependencies = {};
+  };
+
+  window.stik.boundary( { as: "$h", to: helpers } );
+}());
+
+window.stik.boundary( { as: "$window", to: window } );
+
+window.stik.helper( "$window", function(){
+  return window;
+});
+
+window.stik.helper( "debounce", function(){
+  return function debounce( func, wait, immediate ){
+    // copied from underscore.js
+    var timeout;
+    return function(){
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if ( !immediate ) func.apply( context, args );
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout( timeout );
+      timeout = setTimeout( later, wait );
+      if ( callNow ) func.apply( context, args );
+    };
+  };
+});
+
+window.stik.helper( "goTo", function( $window ){
+  return function goTo( url ){
+    $window.location = url;
+  };
+});
+
+window.stik.helper( "deepExtend", function(){
+  return function deepExtend( destination, source ){
+    for ( var property in source ) {
+      if ( Object.isObjectLiteral( destination[ property ] ) && Object.isObjectLiteral( source[ property ] ) ) {
+        destination[ property ] = destination[ property ] || {};
+        arguments.callee( destination[ property ], source[ property ]);
+      } else {
+        destination[ property ] = source[ property ];
+      }
+    }
+    return destination;
+  };
+});
